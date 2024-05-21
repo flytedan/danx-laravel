95349610e3fa4a096adf68701fa5781fcd06be91
Daniel Newman <dan@flytedesk.com>
2024-05-21 12:43:56 -0300

    adding storables to stored_files migration

--- database/migrations/0000_danx_files_tables.php
+++ database/migrations/0000_danx_files_tables.php
@@ -20,14 +20,27 @@ return new class extends Migration {
 				$table->json('exif')->nullable();
 				$table->json('meta')->nullable();
 				$table->json('location')->nullable();
-				$table->string('category', 255)->nullable();
 				$table->string('transcode_name')->nullable();
 				$table->uuid('original_stored_file_id')->nullable();
 				$table->timestamps();
 				$table->timestamp('deleted_at')->nullable();
 
-				$table->index(['storable_id', 'storable_type'], 'file_storable_index');
 				$table->foreign('original_stored_file_id')->references('id')->on('stored_files');
+			});
+		}
+
+		if (!Schema::hasTable('stored_file_storables')) {
+			Schema::create('stored_file_storables', function (Blueprint $table) {
+				$table->bigIncrements('id');
+				$table->uuid('stored_file_id');
+				$table->morphs('storable');
+				$table->string('category', 255)->nullable();
+				$table->timestamps();
+				$table->unique(['storable_id', 'storable_type', 'stored_file_id'], 'stored_file_storables_unique');
+			});
+
+			Schema::table('stored_file_storables', function (Blueprint $table) {
+				$table->foreign('stored_file_id')->references('id')->on('stored_files')->cascadeOnDelete();
 			});
 		}
 	}
@@ -35,5 +48,6 @@ return new class extends Migration {
 	public function down()
 	{
 		Schema::dropIfExists('stored_files');
+		Schema::dropIfExists('stored_file_storables');
 	}
 };
